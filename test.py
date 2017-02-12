2afdefad26bff1993f7caa8e5e907615924b13fc
Danish Kamal <danish8802204230@gmail.com>
2017-02-13 00:53:00 +0530

    Update

--- test.py
+++ test.py
@@ -11,18 +11,28 @@ def get_mfcc_features(file):
 	fbank_feat = logfbank(sig,rate)
 	print(fbank_feat[1:3,:])
 
-rootdir = os.getcwd() + '/DataSet/'
-for subdir, dirs, files in os.walk(rootdir):
-	call(['mkdir', subdir.replace("au", "wav")])
-	for file in files:
-		path = os.path.join(subdir, file)
-		# if(path.split('.')[-1] == 'au'):
-		# 	# print(path)
-		# 	new_path = path.replace("au", "wav")
-		# 	# print(new_path)
-		# 	call(['sox', path , '-e', 'signed-integer', new_path])
 
-		if(path.split('.')[-1] == 'wav'):
-			get_mfcc_features(path)
+def convert_au_to_wav():
+	rootdir = os.getcwd() + '/DataSet/'
+	for subdir, dirs, files in os.walk(rootdir):
+		call(['mkdir', subdir.replace("au", "wav")])
+		for file in files:
+			path = os.path.join(subdir, file)
+			if(path.split('.')[-1] == 'au'):
+				# print(path)
+				new_path = path.replace("au", "wav")
+				# print(new_path)
+				call(['sox', path , '-e', 'signed-integer', new_path])
 
 
+def loop_through_dataset():
+	rootdir = os.getcwd() + '/DataSet/'
+	for subdir, dirs, files in os.walk(rootdir):
+		for file in files:
+			path = os.path.join(subdir, file)
+			if(path.split('.')[-1] == 'wav'):
+				get_mfcc_features(path)
+
+
+# convert_au_to_wav()
+loop_through_dataset()
